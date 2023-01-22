6d6ac80f67b3fbf8b5d8c0e2d03079644980de6f
James Hoffman <jamesvhoffman@outlook.com>
2023-01-22 17:43:57 -0500

    Path fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: 'thejhoffman.github.io',
+  basePath: '/thejhoffman.github.io',
 };
 
 module.exports = nextConfig;
