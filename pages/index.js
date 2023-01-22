b9b08bd4e6dde5d379b40242b7d1b36f8e47f6cc
James Hoffman <jamesvhoffman@outlook.com>
2023-01-22 17:02:25 -0500

    Altered template

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import About from "../src/components/About";
+import Contact from "../src/components/Contact";
+import Home from "../src/components/Home";
+import News from "../src/components/News";
+import Portfolio from "../src/components/Portfolio";
+import Layout from "../src/layouts/Layout";
+
+const Index = () => {
+  return (
+    <Layout>
+      <Home />
+      <About />
+      <Portfolio />
+      {/* <News /> */}
+      <Contact />
+    </Layout>
+  );
+};
+export default Index;
