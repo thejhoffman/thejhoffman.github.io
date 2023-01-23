aac22a4a9adafb289e842659c63d03ecf96a0243
James Hoffman <jamesvhoffman@outlook.com>
2023-01-23 15:56:16 -0500

    Deploy

--- out/_next/static/56qIk843E86nAQBfqJhpE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/89-68a044ed678d73d8.js","static/chunks/pages/index-92d9ed9fd1f01be7.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
