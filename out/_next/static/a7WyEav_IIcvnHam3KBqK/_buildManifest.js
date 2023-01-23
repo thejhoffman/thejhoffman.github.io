9e14ad1f1160132768edda131690411710bcb4c9
James Hoffman <jamesvhoffman@outlook.com>
2023-01-23 16:00:03 -0500

    Removing files

--- out/_next/static/a7WyEav_IIcvnHam3KBqK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/184-ea15f4128f180d30.js","static/css/f4a75330f7bec053.css","static/chunks/pages/index-22358d3a422694dd.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
