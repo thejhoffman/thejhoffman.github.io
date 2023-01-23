60a10dbacc6cbba1ca4d8f4040b918c469564b52
James Hoffman <jamesvhoffman@outlook.com>
2023-01-22 21:47:10 -0500

    Fixed clear warning

--- src/waves.js
+++ src/waves.js
@@ -462,7 +462,7 @@ export const waves = () => {
       this.uniforms.time = elapsed;
 
       if (this.count > 0) {
-        gl.clear(gl.COLORBUFFERBIT);
+        gl.clear(gl.COLOR_BUFFER_BIT);
         gl.drawArrays(gl.POINTS, 0, this.count);
       }
 
